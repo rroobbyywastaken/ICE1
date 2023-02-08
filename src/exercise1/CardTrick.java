2da88c2617be92258aa3f877ae2bfc8a567a2162
rroobbyywastaken <122491217+rroobbyywastaken@users.noreply.github.com>
2023-02-08 16:27:00 -0500

    changedPrintInfo

--- src/exercise1/CardTrick.java
+++ src/exercise1/CardTrick.java
@@ -56,19 +56,19 @@ public class CardTrick {
         System.out.println("Congratulations, you guessed right!");
         System.out.println();
         
-        System.out.println("My name is Paul, but you can call me prof, Paul or sir");
+        System.out.println("My name is Rishabh Kumar, but you can call me Robby!");
         System.out.println();
         
         System.out.println("My career ambitions:");
-        System.out.println("-- Be more active on LinkedIn");
-        System.out.println("-- Have a semester with no violations of academic integrity!");
+        System.out.println("-- Become a Gaming Character Designer or be assiociated with gaming in any manner.");
+        System.out.println("-- Gaming!");
 	System.out.println();	
 
         System.out.println("My hobbies:");
-        System.out.println("-- Investing");
-        System.out.println("-- Cooking");
-        System.out.println("-- Reading/Watching TV");
-        System.out.println("-- Riding my motorcycle");
+        System.out.println("-- Gaming!");
+        System.out.println("-- Listening music");
+        System.out.println("-- Watching anime");
+        System.out.println("-- Enjoying my days off with my friends");
 
         System.out.println();
         
